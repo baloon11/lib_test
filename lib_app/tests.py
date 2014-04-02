cb1b78b08bf48cf2f0c912b4bd4813891547cb07
Mikita Vorobyov <olfb_c@ukr.net>
2014-04-02 21:27:32 +0300

    3 commit--new tests

--- lib_app/tests.py
+++ lib_app/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+  
+class BookListView_Test(TestCase):
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200) 
 
-# Create your tests here.
+class Book_Detail_Test(TestCase):                                                            
+    def test_book_detail(self):                                                                      
+        response = self.client.get('/book/1/') 
+        self.assertEqual(response.status_code, 200) 
+
+class Book_Edit_Test(TestCase):                                                                                
+    def test_book_edit(self):                                               
+        response = self.client.post('/book_edit/10/',{'title': 'Hamlet',
+                                                      'author': 'William Shakespeare'})
+        self.assertEqual(response.status_code,200)
+
+                                         
